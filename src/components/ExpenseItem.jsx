45784571f72153ec1f6bc4cae373ac5ad3bd700d
Reinar Kajava <reinar.kajava@voco.ee>
2025-02-12 11:12:43 +0200

    Ylesanne 8. Ymbritseva Komponendi rakendamine

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,9 +1,10 @@
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
+import Card from './Card.jsx'
 //Korrastatud 
 const ExpenseItem = (props) => {
     return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
         <div>
             <ExpenseDate date={props.data.date}/>
         </div>
@@ -11,7 +12,7 @@ const ExpenseItem = (props) => {
             <h2>{props.data.title}</h2>
         </div>
         <div className='expense-item__price'>{props.data.price}</div>
-    </div>
+    </Card>
 
     )
 }
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem.jsx';
 import './Expenses.css';
+import Card from './Card.jsx';
 
 const Expenses = (props) => {
   // Kontrollime saabunud props-e, et näha, mis andmed komponendile jõuavad.
   console.log(props);
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.expenses.map((expense) => (
         <ExpenseItem
           key={expense.id} // Unikaalne võti iga elemendi jaoks.
           data={expense} // Saadame ExpenseItem-le ühe kulu objekti.
         />
       ))}
-    </div>
+    </Card>
   );
 };
 
